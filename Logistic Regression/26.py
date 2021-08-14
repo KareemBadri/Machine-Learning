8ad950d3f155ee96ddaa03fbc13e9ed65b0bcbd3
Kareem Badri <87897235+KareemBadri@users.noreply.github.com>
2021-08-14 14:16:08 +0300

    Add files via upload

---
+++ Logistic Regression/26.py
@@ -0,0 +1,40 @@
+
+"""
+Created on Fri Aug 13 18:25:29 2021
+
+@author: Karim Badri
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset=pd.read_csv('data.csv')
+X=dataset.iloc[:,[2,3]].values
+y=dataset.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier=LogisticRegression(random_state=0)
+classifier.fit(X_train,y_train)
+
+y_pred=classifier.predict(X_test)
+
+#confusion matrix shows accurate and summarze comparision
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+
+
+
+
+
+
+
+
